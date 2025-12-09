d9a7fedb92df0f3c0187d570843edd2ef65d1dda
fallby <121184597+fallby@users.noreply.github.com>
2025-12-09 21:41:45 +0400

    Update VigenerCipher.js

--- 2 lab/VigenerCipher.js
+++ 2 lab/VigenerCipher.js
@@ -1,26 +1,31 @@
 const alphabet = ['А', 'Б', 'В', 'Г', 'Д', 'Е', 'Ё', 'Ж', 'З', 'И', 'Й', 'К', 'Л', 'М', 'Н', 'О', 'П', 'Р', 'С', 'Т', 'У', 'Ф', 'Х', 'Ц', 'Ч', 'Ш', 'Щ', 'Ъ', 'Ы', 'Ь', 'Э', 'Ю', 'Я'];
 const keys = ["КОД", "КЛЮЧ", "ШИФР", "БАЙТ", "ФАЙЛ", "ВЗЛОМ", "ХАКЕР", "СЕРВЕР", "ПАРОЛЬ", "ЛОГИН", "АККАУНТ", "ПРОГРАММА", "СИСТЕМА", "ДАННЫЕ"];
 
-let encryptedText = document.getElementById('encrypted-output');
-let key = document.getElementById('encrypt-key-display');
-let encryptButton = document.getElementById("encrypt-button");
+const textInput = document.getElementById('text-input');
+const resultOutput = document.getElementById('result-output');
+const keyInput = document.getElementById('key-input');
+const keyOutput = document.getElementById('key-output');
 
-function VigenerCipher() {
-    let originalText = document.getElementById('text-encrypt').value;
-    let k = keys[Math.ceil(Math.random() * keys.length)];
+const encryptButton = document.getElementById("encrypt-button");
+const decryptButton = document.getElementById("decrypt-button");
+
+function encryptVigenerCipher() {
+    let originalText = textInput.value.toUpperCase().replace(/[^А-ЯЁ]/g, '');;
     let keyWord = "";
     let result = [];
     let textLength = originalText.length;
-    let keyLength = k.length;
-    originalText = originalText.toUpperCase();
-    originalText.split("");
 
+    if (keyInput.value === "") {
+        keyWord = keys[Math.floor(Math.random() * keys.length)].toUpperCase();
+    } else {
+        keyWord = keyInput.value.toUpperCase();
+    }
 
-    let count = Math.round(textLength / keyLength);
-    keyWord = k.repeat(count);
+    let keyLength = keyWord.length;
+    let count = Math.ceil(textLength / keyLength);
+    keyWord = keyWord.repeat(count);
     keyWord = keyWord.slice(0, textLength);
-
-
+    
     for (let i = 0; i < textLength; i++) {
         let indexWord = alphabet.indexOf(originalText[i], 0);
         let indexKey = alphabet.indexOf(keyWord[i], 0);
@@ -29,8 +34,39 @@ function VigenerCipher() {
     }
 
     let resultText = result.join("");
-    encryptedText.innerHTML = resultText;
-    key.innerHTML = keyWord;
+    resultOutput.innerHTML = resultText;
+    keyOutput.innerHTML = keyWord;
 }
 
-encryptButton.addEventListener('click', VigenerCipher);
+function decryptVigenerCipher() {
+    let originalText = textInput.value.toUpperCase().replace(/[^А-ЯЁ]/g, '');;
+    let keyWord = keyInput.value.trim().toUpperCase();
+    let result = [];
+    let textLength = originalText.length;
+
+    if (keyWord.length === 0) {
+        resultOutput.innerHTML = 'Введите ключ для расшифровки!';
+        return;
+    }
+
+    let keyLength = keyWord.length;
+    let count = Math.ceil(textLength / keyLength);
+    keyWord = keyWord.repeat(count);
+    keyWord = keyWord.slice(0, textLength);
+
+
+    for (let i = 0; i < textLength; i++) {
+        let indexWord = alphabet.indexOf(originalText[i]);
+        let indexKey = alphabet.indexOf(keyWord[i]);
+        let newIndex = (indexWord - indexKey + alphabet.length) % alphabet.length;
+        result.push(alphabet[newIndex]);
+    }
+
+    let resultText = result.join("");
+    resultOutput.innerHTML = resultText;
+    keyOutput.innerHTML = keyWord;
+}
+
+
+encryptButton.addEventListener('click', encryptVigenerCipher);
+decryptButton.addEventListener('click', decryptVigenerCipher);
